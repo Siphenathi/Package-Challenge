b6c6040cce9edf5dfeaaeb1724e88fa7a1ebdd12
Siphenathi <spantshwa.lukho@gmail.com>
2023-02-28 02:17:24 +0200

    Done with validation and constraints

---
+++ Mobiquity.Packer/Mobiquity.Packer/Model/ApiException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Mobiquity.Packer.Model
+{
+	public class ApiException : Exception
+	{
+		public ApiException(string message) : base(message)
+		{
+		}
+	}
+}
---
+++ Mobiquity.Packer/Mobiquity.Packer/Model/Package.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Mobiquity.Packer.Model
+{
+	public class Package
+	{
+		public int PackageLimit { get; set; }
+		public List<Item> Items { get; set; }
+	}
+
+	public class Item
+	{
+		public int Index { get; set; }
+		public double Weight { get; set; }
+		public double Cost { get; set; }
+	}
+}
